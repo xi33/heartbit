fa549bde7d9dc73ff7b679cbd836ebef6d394125
Pieux Xi <pieuxxi@gmail.com>
2012-05-28 15:28:40 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heartbit::Application.config.secret_token = 'bdbb701475ed0ccf30db5d603abcd274ce5e3bd1e7c2f309ba5c8a978b86a4249bbe3df3bf7f6cbbd4a26bbf24a03c9f6f8cecdfe17c71e2f148388349042f02'
